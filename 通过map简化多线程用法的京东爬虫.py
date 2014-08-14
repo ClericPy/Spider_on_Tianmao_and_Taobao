d8b4c8c3d035583d5f3bc6f04163cbe700f05e2c
lidongone <lidongone@qq.com>
2014-08-14 18:26:45 +0800

    改改注释

--- 通过map简化多线程用法的京东爬虫.py
+++ 通过map简化多线程用法的京东爬虫.py
@@ -7,11 +7,12 @@ import requests
 pip install requests
 '''
 
-# ll是用来存放评论内容列表的，key是页码，value是存放10个当页评论的列表
+# ll是用来存放评论内容列表的，其中key是页码，value是存放10个当页评论的列表
 
 ll = {}
 
-pid = '967821'
+pid = '967821'# 商品ID
+# 模拟请求头
 headers1 = {'GET': '',
             'Host': "club.jd.com",
             'User-Agent': "Mozilla/5.0 (Windows NT 6.2; rv:29.0) Gecko/20100101 Firefox/29.0",
@@ -19,13 +20,13 @@ headers1 = {'GET': '',
 
 r1 = requests.get(
     'http://club.jd.com/productpage/p-{}-s-0-t-3-p-{}.html'.format(pid, 0), headers=headers1)
-
+# 计算最大页码数
 maxpagenum = r1.json()['productCommentSummary'][
-    'commentCount'] // 10  # 先获取最大页码数
+    'commentCount'] // 10  
 
 
 def getrate_jd(pagenum):
-    global pid  # 这里用全局是因为写这个脚本的时候对map不太熟练，其实多参数可以用元组的
+    global pid  # 这里用全局是因为写这个脚本的时候对map不太熟练，其实多参数可以用元组的，但是嫌麻烦，以后可以优化一下
     '''该函数用来获取商品ID是pid的第pagenum页的评论列表'''
     headers1 = {'GET': '',
                 'Host': "club.jd.com",
